7c4f0f4f8c5bcd0362fbc2c7b62a0974718b03ca
hauomu <113390452+hauomu@users.noreply.github.com>
2026-06-12 01:12:04 +0800

    Delete check_date_format.py

--- check_date_format.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data_exports/deliveries_feedback_merged.csv', encoding='latin-1', nrows=5)
-print("Sample data:\n")
-print(df[['booking_datetime', 'promised_delivery_datetime', 'delivery_datetime']].head(10))
-print("\n\nData types:")
-print(df.dtypes)
